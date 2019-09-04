2748c8e9124086a05960a2014e7fb8a987483530
liars <lars27399@gmail.com>
2019-09-04 22:52:13 +0700

    read file excel after submit use xlsx library and write it to database

--- admin/index.php
+++ admin/index.php
@@ -74,6 +74,65 @@
             return promise;
         }
 
+        let readFileAfterSubmit = (url) => {
+            let fetchURL = url.des_file;
+            let promise = new Promise( (resolve, reject) => {
+                let xmlhttp = new XMLHttpRequest();
+                xmlhttp.responseType = "arraybuffer";
+                xmlhttp.onreadystatechange = function(e) {
+                    if(this.status === 200 && this.readyState === 4) {
+                        if(this.response) {
+                            let arraybuffer = this.response;
+                
+                            /* convert data to binary string */
+                            let data = new Uint8Array(arraybuffer);
+                            let arr = new Array();
+                            for (let i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+                            let bstr = arr.join("");
+                            /* Call XLSX */
+                            let workbook = XLSX.read(bstr, {
+                                type: "binary"
+                            });
+                
+                            let first_sheet_name = workbook.SheetNames[0];
+                            let worksheet = workbook.Sheets[first_sheet_name];
+                            resolve(XLSX.utils.sheet_to_json(worksheet, {raw: true}));
+                        } else {
+                            reject("Can not read file");
+                        }
+                    }
+                }
+
+                xmlhttp.open("GET", fetchURL, true);
+                xmlhttp.send();
+            }) 
+            return promise;
+        }
+
+        let writeDataToDb = (data) => {
+            let formData = new FormData();
+            let type = document.querySelector("input[type=radio]:checked").value;
+            formData.append("json", JSON.stringify(data));
+            formData.append("type", type);
+            
+            let promise = new Promise( (resolve, reject) => {
+                let xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function () {
+                    if(this.status === 200 && this.readyState === 4) {
+                        console.log(this.response)
+                        if(this.response) {
+                            let res = JSON.parse(this.response);
+                            resolve(res);
+                        } else {
+                            reject("Can not write file to DB");
+                        }
+                    }
+                }
+                xmlhttp.open("POST", "./write.php", true);
+                xmlhttp.send(formData);
+            })
+            return promise;
+        }
         
 
     </script>
